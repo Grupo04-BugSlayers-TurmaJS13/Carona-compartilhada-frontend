c2c2a0431d425aceb874d28255c9eaa166afcdaf
Gaalmeida-dev <gabrielalmeidaescalera@gmail.com>
2026-04-22 16:18:42 -0300

    ✨ New Feature: Navbar + Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AboutProject from "./pages/sobre/AboutProject";
 import AboutUs from "./pages/sobre/AboutUs";
-
+import Footer from "./components/footer/Footer";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<AboutProject />} />
         <Route path="/sobre" element={<AboutProject />} />
         <Route path="/sobrenos" element={<AboutUs />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   );
 }
